4b677838b4ed905323426ea9d78030e151ebc095
Hampus Londögård <hampus.londogard@gmail.com>
2021-01-22 15:37:38 +0000

    Adding imports

--- src/jsMain/kotlin/mainJs.kt
+++ src/jsMain/kotlin/mainJs.kt
@@ -1,3 +1,12 @@
+import kotlinx.browser.document
+import kotlinx.browser.window
+import org.w3c.dom.*
+import org.w3c.dom.events.KeyboardEvent
+
+const val background = "#000000"
+const val apple = "#ff3232"
+const val snake = "#00ff00"
+const val snakeHead = "#2c7507"
 
 fun main() {
     println("Hello World!")
--- src/jvmMain/kotlin/main.kt
+++ src/jvmMain/kotlin/main.kt
@@ -1,3 +1,10 @@
+import java.awt.*
+import java.awt.event.KeyAdapter
+import java.awt.event.KeyEvent
+import java.awt.event.KeyEvent.*
+import javax.swing.JFrame
+import javax.swing.JPanel
+import kotlin.concurrent.fixedRateTimer
 
 fun main() {
     println("Hello World!")
